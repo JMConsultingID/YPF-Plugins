b9c805e3a4fb5d13784e0f9df9df78974ef859e9
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-09 19:05:36 +0800

    Update ypf-plugins-widget-pricing-table.php

--- elementor/widgets/ypf-plugins-widget-pricing-table.php
+++ elementor/widgets/ypf-plugins-widget-pricing-table.php
@@ -99,7 +99,7 @@ class Elementor_YpfPlugins_Widget_Pricing_Table extends \Elementor\Widget_Base {
                     $sub_field_name = $sub_field['name'];
                     // The value is in the values array
                     $sub_field_value = $step_1_fx_challenge[$sub_field['name']];
-                    echo '<div class="pt__row "'. esc_html($sub_field_name) . '">' . esc_html($sub_field_label) . '</div>';
+                    echo '<div class="pt__row '. esc_html($sub_field_name) . '">' . esc_html($sub_field_label) . '</div>';
                 }
             }
             ?>
