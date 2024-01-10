e3d31d8c1d65cf2b9e1da2814c7bade0ec89b983
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-11 00:43:36 +0800

    Update ypf-plugins-widget-pricing-table-product.php

--- elementor/widgets/ypf-plugins-widget-pricing-table-product.php
+++ elementor/widgets/ypf-plugins-widget-pricing-table-product.php
@@ -50,11 +50,6 @@ class Elementor_YpfPlugins_Widget_Pricing_Table_Per_Product extends \Elementor\W
 	}
 
 	protected function render() {
-
-	// Check if Elementor editor is active
-    if (\Elementor\Plugin::$instance->editor->is_edit_mode()) {
-        echo '<p>Elementor editor is active. Product details will be displayed on the frontend.</p>';
-    } else {
 	// Get the selected product ID from the widget settings
 	$settings = $this->get_settings_for_display();
 	$selected_product_id = $settings['selected_product'];
@@ -207,7 +202,6 @@ class Elementor_YpfPlugins_Widget_Pricing_Table_Per_Product extends \Elementor\W
 		} else {
         echo '<p>Please select a product.</p>';
     }
-	}
 	}
 
     // Helper function to get WooCommerce products
