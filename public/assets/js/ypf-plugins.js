f4b267056eeace5816ff333832715265201ab38d
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-11 12:54:15 +0800

    Update ypf-plugins.js

--- public/assets/js/ypf-plugins.js
+++ public/assets/js/ypf-plugins.js
@@ -95,7 +95,7 @@ document.querySelectorAll('.tab-nav-list li').forEach(function(tabButton, index)
   });
 });
 
-tippy('button', {
+tippy('.data-template', {
         content(reference) {
             const id = reference.getAttribute('data-template');
             const template = document.getElementById(id);
