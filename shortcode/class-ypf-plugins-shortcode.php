5f5a195bbdbb22a93c1998822f69f5ea553225d0
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-13 00:07:18 +0800

    update tooltips

--- shortcode/class-ypf-plugins-shortcode.php
+++ shortcode/class-ypf-plugins-shortcode.php
@@ -8,6 +8,8 @@ function ypf_pricing_table_shortcode( $atts ) {
     ), $atts );
 
     $selected_product_id = $atts['productid'];
+    $tooltips_post = get_option('ypf_select_post_tooltips');
+    $tooltips_post_id = isset($tooltips_post) ? $tooltips_post : '861';
 
     if ( ! empty( $selected_product_id ) ) {
         $product = wc_get_product( $selected_product_id );
@@ -19,7 +21,7 @@ function ypf_pricing_table_shortcode( $atts ) {
                 ?>
                 <div class="pricing__table product-<?php echo $selected_product_id; ?>">
                 <div class="pt__title">
-                    <?php display_acf_group_labels_and_tooltips('step_1:_fx_challenge', 'fx_challenge_tooltips', $selected_product_id); ?>
+                    <?php display_acf_group_labels_and_tooltips('step_1:_fx_challenge', 'fx_challenge_tooltips', $selected_product_id, $tooltips_post_id); ?>
                 </div>
 
 
@@ -87,7 +89,7 @@ function ypf_pricing_table_shortcode( $atts ) {
 }
 add_shortcode( 'ypf-pricing-table', 'ypf_pricing_table_shortcode' );
 
-function display_acf_group_labels_and_tooltips($group_field_name, $tooltips_field_name, $product_id, $tooltips_post_id = 860) {
+function display_acf_group_labels_and_tooltips($group_field_name, $tooltips_field_name, $product_id, $tooltips_post_id) {
     // Fetch group field values and object for the product
     $group_field_values = get_field($group_field_name, $product_id);
     $group_field_object = get_field_object($group_field_name, $product_id);
