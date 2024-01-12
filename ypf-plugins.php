064d523ffe1ac867b0327145b62be7a3c643d4be
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-12 23:27:38 +0800

    Update ypf-plugins.php

--- ypf-plugins.php
+++ ypf-plugins.php
@@ -165,9 +165,14 @@ add_action( 'wp_enqueue_scripts', 'ypf_plugins_widgets', );
 
 function check_for_shortcode_and_enqueue_scripts() {
     if ( get_option('ypf_enable_pricing_table') ) {
+        global $post;
+
+        // Check if the current post exists and contains your shortcode
+        if (is_a($post, 'WP_Post') && has_shortcode($post->post_content, 'ypf-pricing-table')) {
             // Enqueue scripts and styles here
             wp_enqueue_style('ypf-plugins-css');
             wp_enqueue_script('ypf-plugins-js');
+        }
     }
 }
 add_action('wp', 'check_for_shortcode_and_enqueue_scripts');
