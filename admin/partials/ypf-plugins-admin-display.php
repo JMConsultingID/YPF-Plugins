4c5699e4ba0f509e135eb369f4352143e9928f27
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-12 11:46:09 +0800

    Update ypf-plugins-admin-display.php

--- admin/partials/ypf-plugins-admin-display.php
+++ admin/partials/ypf-plugins-admin-display.php
@@ -1,6 +1,5 @@
 <div class="wrap">
     <h1>YPF Plugins Configuration Page</h1>
     <p>This configuration page is currently under development. For setting up table pricing, please visit the <a href="admin.php?page=ypf-plugins-pricing-table">YPF Pricing Table setup page</a>.</p>
-    <p>This plugin requires the <a href="https://wordpress.org/plugins/advanced-custom-fields/" target="_blank">Advanced Custom Fields Plugin</a>. Please ensure it is installed and activated. here is to download the default configuration file for the plugin. <a href="https://staging.finpr.com/upload/import-settings-acf.js">Download here</a>.</p>
-    <p>If you have any questions or need assistance, feel free to contact the developer at <a href="mailto:ka@finpr.com">developer finPR</a>.</p>
+    <p>If you have any questions or need assistance, Please visit our Support Page<a href="https://yourpropfirm.com/">yourpropfirm.com</a> or Open an issue in the <a href="https://github.com/JMConsultingID/ypf-plugins">[GitHub repository]</a>.</p>
 </div>
