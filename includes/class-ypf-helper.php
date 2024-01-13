3db0b07d18f69f2a941a2bbd44eb04c79db1ef2f
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-14 00:27:23 +0800

    Update class-ypf-helper.php

--- includes/class-ypf-helper.php
+++ includes/class-ypf-helper.php
@@ -101,7 +101,7 @@ function display_acf_group_labels_and_tooltips_el($group_field_name, $tooltips_f
                     $sub_field_tooltip_text = '<span class="data-template" data-template="'. esc_html($sub_field_tooltips_name) . '"><i aria-hidden="true" class="fas fa-info-circle"></i></span>';
                 }
             }
-            echo '<div class="pt__row heading-vertical ' . esc_html($sub_field_name) . ' ypf_side_title_bg_elementor ypf_side_title_text_elementor pt__table_general_border_color"><div class="pt__row-heading-text">' . esc_html($sub_field_label) . $sub_field_tooltip_text . '</div></div>'; 
+            echo '<div class="pt__row heading-vertical ' . esc_html($sub_field_name) . ' ypf_side_title_bg_elementor ypf_side_title_text_elementor pt__table_general_border" style="border: solid 1px #E19287;"><div class="pt__row-heading-text">' . esc_html($sub_field_label) . $sub_field_tooltip_text . '</div></div>'; 
         }
 
         echo '<div style="display: none;">';
@@ -135,7 +135,7 @@ function display_acf_group_fields_el($group_field_name, $product_id, $css_class_
             // Determine the class to add based on whether it's the first item
             $additional_class = $is_first_item ? 'ypf_step_title_bg_elementor ypf_step_title_text_elementor' : 'ypf_table_content_bg_elementor ypf_table_content_text_elementor';
 
-            echo '<div class="pt__row ' . esc_attr($css_class_prefix) . ' val val-' . esc_attr($sub_field_name) . ' ' . $additional_class . ' pt__table_general_border_color">' . $sub_field_value . '</div>';
+            echo '<div class="pt__row ' . esc_attr($css_class_prefix) . ' val val-' . esc_attr($sub_field_name) . ' ' . $additional_class . ' pt__table_general_border" style="border: solid 1px #E19287;">' . $sub_field_value . '</div>';
             $is_first_item = false; // After the first iteration, set this flag to false
         }
     }
