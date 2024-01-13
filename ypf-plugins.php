d9894eebaea8aa4d073ab3e8035e44d42e25b3b3
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-13 19:04:00 +0800

    Update ypf-plugins.php

--- ypf-plugins.php
+++ ypf-plugins.php
@@ -257,13 +257,13 @@ function display_acf_group_fields_el($group_field_name, $product_id, $css_class_
 function display_swiper_navigation_buttons_el($left_button_id, $right_button_id) {
     ?>
     <div class="pt__option__mobile__nav">
-        <a id="<?php echo esc_attr($left_button_id); ?>" href="#" class="mobile__nav__btn swiper-button-prev">
+        <a id="<?php echo esc_attr($left_button_id); ?>" href="#" class="mobile__nav__btn">
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M22.1538 11.9819H1.81972" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                 <path d="M11.9863 22.1535L1.82043 11.9865L11.9863 1.81946" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
         </a>
-        <a id="<?php echo esc_attr($right_button_id); ?>" href="#" class="mobile__nav__btn swiper-button-next">
+        <a id="<?php echo esc_attr($right_button_id); ?>" href="#" class="mobile__nav__btn">
             <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M1.81934 11.9819H22.1534" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                 <path d="M11.9863 22.1535L22.1522 11.9865L11.9863 1.81946" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
