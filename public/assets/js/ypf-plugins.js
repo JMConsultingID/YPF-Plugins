f17c00d1e823125dd1d55f0958b65706229ef82f
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-16 23:07:23 +0800

    Update ypf-plugins.js

--- public/assets/js/ypf-plugins.js
+++ public/assets/js/ypf-plugins.js
@@ -1,33 +1,3 @@
-(function( $ ) {
-	'use strict';
-
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
 jQuery(document).ready(function($) {
     var pricingCardSwiper;
     var pricingCardSwiperSingle;
@@ -35,7 +5,7 @@ jQuery(document).ready(function($) {
     var initSwiperSingle = false;
     var currentSlideIndex = 0;
 
-    $( window ).load(function() {
+    window.addEventListener('load', function() {
         // Function to initialize Swiper for #pricingTableSlider
         function initializeSwiper() {
             if (pricingCardSwiper) {
@@ -134,5 +104,3 @@ jQuery(document).ready(function($) {
         });
     });
 });
-
-})( jQuery );
