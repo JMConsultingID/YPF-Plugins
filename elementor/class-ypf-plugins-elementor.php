ef779fcd82e8617dd5eefe7713325ecab10137bf
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-18 00:02:17 +0800

    Update class-ypf-plugins-elementor.php

--- elementor/class-ypf-plugins-elementor.php
+++ elementor/class-ypf-plugins-elementor.php
@@ -16,10 +16,12 @@ function register_ypfplugins_widget( $widgets_manager ) {
 
     // require_once( __DIR__ . '/widgets/ypf-plugins-widget-pricing-table.php' );
     require_once( __DIR__ . '/widgets/ypf-plugins-widget-pricing-table-product.php' ); 
+    require_once( __DIR__ . '/widgets/ypf-plugins-widget-pricing-table-product-single-step.php' );  
 
     
 
     // $widgets_manager->register( new \Elementor_YpfPlugins_Widget_Pricing_Table() ); 
     $widgets_manager->register( new \Elementor_YpfPlugins_Widget_Pricing_Table_Per_Product() );
+    $widgets_manager->register( new \Elementor_YpfPlugins_Widget_Pricing_Table_Single_Product() ); 
 }
 add_action( 'elementor/widgets/register', 'register_ypfplugins_widget' );
