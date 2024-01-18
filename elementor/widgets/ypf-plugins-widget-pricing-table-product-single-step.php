cbdd31c3dffafedde0cca870c2b8504eb54fb60a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-18 23:55:53 +0800

    update core

--- elementor/widgets/ypf-plugins-widget-pricing-table-product-single-step.php
+++ elementor/widgets/ypf-plugins-widget-pricing-table-product-single-step.php
@@ -452,11 +452,13 @@ class Elementor_YpfPlugins_Widget_Pricing_Table_Single_Product extends \Elemento
     		$step_button_switch_first = $first_item['step_button_switch'];
     		if ($first_item && !empty($first_item['acf_group_field'])) { ?>
 			  	<div class="pt__title tooltips-id-<?php echo $tooltips_post_id_elementor;  ?>">
+			  		<div class="pt__title__wrap">
 	                <?php display_acf_group_labels_and_tooltips_el($first_item['acf_group_field'], 'fx_challenge_tooltips', $first_item['selected_product'], $tooltips_switch , $tooltips_post_id_elementor); 
 	                if ($step_button_switch_first == 'yes') {
 		            	echo '<div class="pt__row heading-vertical"><div class="pt__row-heading-text"></div></div>'; 
 		        	}
 	                ?>
+	           		</div>
 	            </div>
 	        <?php 
 	         }
--- includes/class-ypf-helper.php
+++ includes/class-ypf-helper.php
@@ -50,8 +50,6 @@ function display_acf_group_labels_and_tooltips($group_field_name, $tooltips_fiel
     $tooltips_field_values = get_field($tooltips_field_name, $tooltips_post_id);
 
     if ($group_field_object) {
-        echo '<div class="pt__title__wrap">';
-
         foreach ($group_field_object['sub_fields'] as $index => $sub_field) {
             $sub_field_label = $sub_field['label'];
             $sub_field_name = $sub_field['name'];
@@ -76,8 +74,6 @@ function display_acf_group_labels_and_tooltips($group_field_name, $tooltips_fiel
             }
           }
           echo '</div>';
-
-        echo '</div>'; // Close pt__title__wrap
     }
 }
 
