92e08910a8c153842814e2589aec3a1392949f07
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-01-28 21:29:42 +0800

    Update ypf-plugins-widget-pricing-table-product.php

--- elementor/widgets/ypf-plugins-widget-pricing-table-product.php
+++ elementor/widgets/ypf-plugins-widget-pricing-table-product.php
@@ -351,14 +351,14 @@ class Elementor_YpfPlugins_Widget_Pricing_Table_Per_Product extends \Elementor\W
 	        ]
 	    );
 
-	    $this->add_control(
+	    $this->add_responsive_control(
 			'mobile_general_title_width',
 			[
 				'label' => esc_html__( 'Mobile Column Title (%)', 'ypf-plugins' ),
 				'type' => \Elementor\Controls_Manager::SLIDER,
-				'size_units' => [ '%' ],
+				'size_units' => [ 'px' ],
 				'range' => [
-					'%' => [
+					'px' => [
 						'min' => 0,
 						'max' => 100,
 						'step' => 1,
@@ -371,7 +371,7 @@ class Elementor_YpfPlugins_Widget_Pricing_Table_Per_Product extends \Elementor\W
 			]
 		);
 
-		$this->add_control(
+		$this->add_responsive_control(
 			'mobile_general_content_width',
 			[
 				'label' => esc_html__( 'Mobile Column Content (%)', 'ypf-plugins' ),
